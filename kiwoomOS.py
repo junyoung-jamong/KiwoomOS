52d62cc0f4c64e1c674e50eb696e9d664240d13d
junyoung-jamong <msmapark2@gmail.com>
2018-07-22 21:45:53 +0900

    2018_07_22 update

--- kiwoomOS.py
+++ kiwoomOS.py
@@ -10,16 +10,31 @@ class KiwoomOS:
 
     def __init__(self, kiwoom):
         self.kiwoom = kiwoom
-        self.kiwoom.OnEventConnect.connect(self._api_onEventConnect)
-        self.kiwoom.OnReceiveConditionVer.connect(self._api_onReceiveConditionVer)
+        self.kiwoom.OnEventConnect.connect(self._api_onEventConnect) #로그인 결과 Event
+        self.kiwoom.OnReceiveConditionVer.connect(self._api_onReceiveConditionVer) #조건식 리스트 수신 Event
+        self.kiwoom.OnReceiveTrData.connect(self._api_onReceiveTrData) #TR요청 수신결과 Event
+        self.kiwoom.OnReceiveRealData.connect(self._api_onReceiveRealData) #실시간 데이터 수신 Event
+        self.kiwoom.OnReceiveChejanData.connect(self._api_onReceiveChejanData) #체결 잔고 수신 Event
+        self.kiwoom.OnReceiveTrCondition.connect(self._api_onReceiveTrCondition) #조건검색 결과 Event
+        self.kiwoom.OnReceiveRealCondition.connect(self._api_onReceiveRealCondition) #실시간 조건 수신 Event
+
+        self.kiwoom.dynamicCall("KOA_Functions(QString, QString)", "SetConditionSearchFlag", "AddPrice")
+
+        self._onLogin_observer = [] #로그인 event
+        self._onReceiveTr_observer = [] #TR수신 event
+        self._onReceiveReal_observer = [] #실시간 체결 수신 event
+        self._onReceiveRealExt_observer = [] #실시간 데이터 수신 event
+        self._onAcceptedOrder_observer = [] #주문 접수 event
+        self._onConcludedOrder_observer = [] #주문 체결 수신 event
+        self._onReceiveBalance_observer = [] #잔고 수신 event
+        self._onReceiveCondition_observer = [] #조건검색 수식 event
+        self._onReceiveRealCondition_observer = [] #실시간 조건검색 수신 event
 
     def _api_onEventConnect(self, nErrCode):
-        print('api_onEventConnect()')
         if nErrCode == 0:
             codes = self.kiwoom.dynamicCall("GetCodeListByMarket(QString)", "0")
             codes += self.kiwoom.dynamicCall("GetCodeListByMarket(QString)", "10")
             codes = codes[:-1]
-
             codeList = codes.split(";")
 
             for code in codeList:
@@ -33,7 +48,7 @@ class KiwoomOS:
                     '종목상태': self.kiwoom.dynamicCall("GetMasterStockState(QString", code)
                 })
 
-            self.kiwoom.dynamicCall("GetConditionLoad()")
+            self.kiwoom.dynamicCall("GetConditionLoad()") #사용자 조건식 리스트 요청
 
     def _api_onReceiveConditionVer(self, lRet, msg):
         conditions = self.kiwoom.dynamicCall("GetConditionNameList()")
@@ -47,7 +62,148 @@ class KiwoomOS:
                 '조건식명': conditionInfo[1]
             })
 
-        print(self.conditionList)
+        for event_func in self._onLogin_observer:
+            event_func(self.stockItemList, self.conditionList)
+
+    # TR데이터 수신
+    def _api_onReceiveTrData(self, sScrNo, sRQName, sTrcode, sRecordName, sPrevNext, nDataLength, sErrorCode,
+                            sMessage, sSplmMsg):
+        for event_func in self._onReceiveTr_observer:
+            event_func(sRQName, sTrcode, sPrevNext)
+
+    # 실시간 데이터 수신
+    def _api_onReceiveRealData(self, sCode, sRealType, sRealData):
+        if sRealType == '주식체결':
+            realData = {
+                '체결시간': self.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, 20),
+                '현재가': self.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, 10),
+                '전일대비': self.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, 11),
+                '등락률': self.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, 12),
+                '최우선매도호가': self.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, 27),
+                '최우선매수호가': self.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, 28),
+                '거래량': self.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, 15),
+                '누적거래량': self.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, 13),
+                '누적거래대금': self.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, 14),
+                '시가': self.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, 16),
+                '고가': self.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, 17),
+                '저가': self.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, 18),
+                '전일대비기호': self.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, 25),
+                '전일거래량대비': self.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, 26),
+                '거래대금증감': self.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, 29),
+                '전일거래량대비': self.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, 30),
+                '거래회전율': self.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, 31),
+                '거래비용': self.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, 32),
+                '체결강도': self.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, 228),
+                '시가총액': self.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, 311),
+                '장구분': self.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, 290),
+                'KO접근도': self.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, 691),
+                '상한가발생시간': self.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, 567),
+                '하한가발생시간': self.kiwoom.dynamicCall("GetCommRealData(QString, int)", sCode, 568)
+            }
+
+            for event_func in self._onReceiveReal_observer:
+                event_func(sCode, realData)
+
+        else:
+            for event_func in self._onReceiveRealExt_observer:
+                event_func(sCode, sRealType)
+
+    # 주문 접수/체결 잔고 수신
+    def _api_onReceiveChejanData(self, sGubun, nItemCnt, sFIdList):
+        if sGubun == '0':
+            orderState = self.kiwoom.dynamicCall("GetChejanData(int)", 913).strip()
+
+            data = {
+                '시간': self.kiwoom.dynamicCall("GetChejanData(int)", 908).strip(),
+                '계좌번호': self.kiwoom.dynamicCall("GetChejanData(int)", 9201).strip(),
+                '주문번호': self.kiwoom.dynamicCall("GetChejanData(int)", 9203).strip(),
+                '원주문번호': self.kiwoom.dynamicCall("GetChejanData(int)", 904).strip(),
+                '종목코드': self.kiwoom.dynamicCall("GetChejanData(int)", 9001).strip(),
+                '종목명': self.kiwoom.dynamicCall("GetChejanData(int)", 302).strip(),
+                '주문수량': self.kiwoom.dynamicCall("GetChejanData(int)", 900).strip(),
+                '주문가격': self.kiwoom.dynamicCall("GetChejanData(int)", 901).strip(),
+                '주문구분': self.kiwoom.dynamicCall("GetChejanData(int)", 905).strip(),
+                '매매구분': self.kiwoom.dynamicCall("GetChejanData(int)", 906).strip(),
+                '매도매수구분': self.kiwoom.dynamicCall("GetChejanData(int)", 907).strip(),
+                '주문업무분류': self.kiwoom.dynamicCall("GetChejanData(int)", 912).strip(),
+                '거부사유': self.kiwoom.dynamicCall("GetChejanData(int)", 919).strip()
+            }
+
+            if orderState == '접수':
+                for event_func in self._onAcceptedOrder_observer:
+                    event_func(data)
+
+            elif orderState == '체결':
+                data['체결번호'] = self.kiwoom.dynamicCall("GetChejanData(int)", 909).strip()
+                data['체결가'] = self.kiwoom.dynamicCall("GetChejanData(int)", 910).strip()
+                data['단위체결가'] = self.kiwoom.dynamicCall("GetChejanData(int)", 914).strip()
+                data['체결누계금액'] = self.kiwoom.dynamicCall("GetChejanData(int)", 903).strip()
+                data['체결량'] = self.kiwoom.dynamicCall("GetChejanData(int)", 911).strip()
+                data['단위체결량'] = self.kiwoom.dynamicCall("GetChejanData(int)", 915).strip()
+                data['당일매매수수료'] = self.kiwoom.dynamicCall("GetChejanData(int)", 938).strip()
+                data['당일매매세금'] = self.kiwoom.dynamicCall("GetChejanData(int)", 939).strip()
+                data['현재가'] = self.kiwoom.dynamicCall("GetChejanData(int)", 10).strip()
+                data['최우선매도호가'] = self.kiwoom.dynamicCall("GetChejanData(int)", 27).strip()
+                data['최우선매수호가'] = self.kiwoom.dynamicCall("GetChejanData(int)", 28).strip()
+
+                for event_func in self._onConcludedOrder_observer:
+                    event_func(data)
+        elif sGubun == '1':
+            data = {
+                '계좌번호': self.kiwoom.dynamicCall("GetChejanData(int)", 9201).strip(),
+                '종목코드': self.kiwoom.dynamicCall("GetChejanData(int)", 9001).strip(),
+                '종목명': self.kiwoom.dynamicCall("GetChejanData(int)", 302).strip(),
+                '보유수량': self.kiwoom.dynamicCall("GetChejanData(int)", 930).strip(),
+                '매입단가': self.kiwoom.dynamicCall("GetChejanData(int)", 931).strip(),
+                '총매입가': self.kiwoom.dynamicCall("GetChejanData(int)", 932).strip(),
+                '주문가능수량': self.kiwoom.dynamicCall("GetChejanData(int)", 933).strip(),
+                '당일순매수량': self.kiwoom.dynamicCall("GetChejanData(int)", 945).strip(),
+                '매수매도구분': self.kiwoom.dynamicCall("GetChejanData(int)", 946).strip(),
+                '당일총매도손익': self.kiwoom.dynamicCall("GetChejanData(int)", 950).strip(),
+                '예수금': self.kiwoom.dynamicCall("GetChejanData(int)", 951).strip(),
+                '기준가': self.kiwoom.dynamicCall("GetChejanData(int)", 307).strip(),
+                '손익률': self.kiwoom.dynamicCall("GetChejanData(int)", 8019).strip(),
+                '당일유가실현손익': self.kiwoom.dynamicCall("GetChejanData(int)", 990).strip(),
+                '당일유가실현손익률': self.kiwoom.dynamicCall("GetChejanData(int)", 991).strip(),
+                '당일신용실현손익': self.kiwoom.dynamicCall("GetChejanData(int)", 992).strip(),
+                '당일신용실현손익률': self.kiwoom.dynamicCall("GetChejanData(int)", 993).strip(),
+                '현재가': self.kiwoom.dynamicCall("GetChejanData(int)", 10).strip(),
+                '최우선매도호가': self.kiwoom.dynamicCall("GetChejanData(int)", 27).strip(),
+                '최우선매수호가': self.kiwoom.dynamicCall("GetChejanData(int)", 28).strip()
+            }
+            for event_func in self._onReceiveBalance_observer:
+                event_func(data)
+
+        elif sGubun == '4':
+            pass
+
+    # 조건검색 결과 수신
+    def _api_onReceiveTrCondition(self, sScrNo, strCodeList, strConditionName, nIndex, nNext):
+        condition = {'조건식인덱스': nIndex, '조건식명': strConditionName}
+        codeList = strCodeList.split(';')
+        itemList = []
+        for code in codeList:
+            if len(code) > 0:
+                codeInfo = code.split('^')
+                itemList.append({
+                    '종목코드': codeInfo[0],
+                    '현재가': int(codeInfo[1])
+                })
+
+        for event_func in self._onReceiveCondition_observer:
+            event_func(condition, itemList)
+
+    # 실시간 조건 편입/이탈 수신
+    def _api_onReceiveRealCondition(self, strCode, strType, strConditionName, strConditionIndex):
+        if strType == 'I':
+            type = '편입'
+        else:
+            type = '이탈'
+
+        condition = {'조건식인덱스': strConditionIndex, '조건식명': strConditionName}
+
+        for event_func in self._onReceiveRealCondition_observer:
+            event_func(condition, strCode, type)
 
     #SetInputValue 호출
     def setInput(self, key, value):
@@ -73,6 +229,10 @@ class KiwoomOS:
     def getTrCount(self, trCode):
         return self.kiwoom.dynamicCall("GetRepeatCnt(QString, QString)", trCode, "")
 
+    #GetCommRealData 호출
+    def getRealData(self, itemCode, fId):
+        return self.kiwoom.dynamicCall("GetCommRealData(QString, int)", itemCode, fId)
+
     #실시간 데이터 등록 관리
     def addRealData(self, codeList):
         if self.getLoginState():
@@ -97,7 +257,63 @@ class KiwoomOS:
                 requestList = requestList[:-1]
                 self.kiwoom.dynamicCall("SetRealReg(QString, QString, QString, QString)", scrNum, requestList, fidList, '1')
 
+    #SendOrder 호출
+    def sendBuyOrder(self, account, itemCode, quantity, price, priceType):
+        self.kiwoom.dynamicCall("SendOrder(QString, QString, QString, int, QString, int, int, QString, QString)",
+                                '신규매수주문',
+                                self._getScreenNumber(),
+                                account,
+                                1,
+                                itemCode,
+                                quantity,
+                                price,
+                                self._getOrderPriceType(priceType),
+                                ''
+                                )
 
+    # SendOrder 호출
+    def sendSellOrder(self, account, itemCode, quantity, price, priceType):
+        self.kiwoom.dynamicCall("SendOrder(QString, QString, QString, int, QString, int, int, QString, QString)",
+                                '신규매도주문',
+                                self._getScreenNumber(),
+                                account,
+                                2,
+                                itemCode,
+                                quantity,
+                                price,
+                                self._getOrderPriceType(priceType),
+                                ''
+                                )
+
+    def _getOrderPriceType(self, type):
+        if type == '지정가':
+            return '00'
+        elif type == '시장가':
+            return '03'
+        elif type == '조건부지정가':
+            return '05'
+        elif type == '최유리지정가':
+            return '06'
+        elif type == '최우선지정가':
+            return '07'
+        elif type == '지정가IOC':
+            return '10'
+        elif type == '시장가IOC':
+            return '13'
+        elif type == '최유리IOC':
+            return '16'
+        elif type == '지정가FOK':
+            return '20'
+        elif type == '시장가FOK':
+            return '23'
+        elif type == '최유리FOK':
+            return '26'
+        elif type == '장전시간외종가':
+            return '61'
+        elif type == '시간외단일가매매':
+            return '62'
+        elif type == '장후시간외종가':
+            return '81'
 
     #SendCondition 호출
     def startConditionMonitoring(self, name, index):
@@ -172,11 +388,16 @@ class KiwoomOS:
     #서버 구분(모의투자 or 실서버)
     def getServerState(self):
         if self.getLoginState():
-            server = self.kiwoom.dynamicCall("GetLoginInfo(QString)", "GetServerGubun")
-            if server == 1:
+            server = self.kiwoom.dynamicCall("GetLoginInfo(QString)", "GetServerGubun").strip()
+            if server == '1':
                 return '모의투자'
             else:
                 return '실서버'
+
+    #계좌비밀번호 설정 창 호출
+    def showAccountWindow(self):
+        if self.getLoginState():
+            self.kiwoom.dynamicCall("KOA_Functions(QString, QString)", "ShowAccountWindow", "")
 
     #화면번호 생성
     def _getScreenNumber(self):
@@ -201,3 +422,76 @@ class KiwoomOS:
             self._realScrNumDict[scrNum] = []
 
         return scrNum
+
+    #Add/Remove Event
+    def addOnLogin(self, func):
+        if func not in self._onLogin_observer:
+            self._onLogin_observer.append(func)
+
+    def removeOnLogin(self, func):
+        if func in self._onLogin_observer:
+            self._onLogin_observer.remove(func)
+
+    def addOnReceiveTr(self, func):
+        if func not in self._onReceiveTr_observer:
+            self._onReceiveTr_observer.append(func)
+
+    def removeOnReceiveTr(self, func):
+        if func in self._onReceiveTr_observer:
+            self._onReceiveTr_observer.remove(func)
+
+    def addOnReceiveReal(self, func):
+        if func not in self._onReceiveReal_observer:
+            self._onReceiveReal_observer.append(func)
+
+    def removeOnReceiveReal(self, func):
+        if func in self._onReceiveReal_observer:
+            self._onReceiveReal_observer.remove(func)
+
+    def addOnReceiveRealExt(self, func):
+        if func not in self._onReceiveRealExt_observer:
+            self._onReceiveRealExt_observer.append(func)
+
+    def removeOnReceiveRealExt(self, func):
+        if func in self._onReceiveRealExt_observer:
+            self._onReceiveRealExt_observer.remove(func)
+
+    def addOnAcceptedOrder(self, func):
+        if func not in self._onAcceptedOrder_observer:
+            self._onAcceptedOrder_observer.append(func)
+
+    def removeOnAcceptedOrder(self, func):
+        if func in self._onAcceptedOrder_observer:
+            self._onAcceptedOrder_observer.remove(func)
+
+    def addOnConcludedOrder(self, func):
+        if func not in self._onConcludedOrder_observer:
+            self._onConcludedOrder_observer.append(func)
+
+    def removeOnConcludedOrder(self, func):
+        if func in self._onConcludedOrder_observer:
+            self._onConcludedOrder_observer.remove(func)
+
+    def addOnReceiveBalance(self, func):
+        if func not in self._onReceiveBalance_observer:
+            self._onReceiveBalance_observer.append(func)
+
+    def removeOnReceiveBalance(self, func):
+        if func in self._onReceiveBalance_observer:
+            self._onReceiveBalance_observer.remove(func)
+
+    def addOnReceiveCondition(self, func):
+        if func not in self._onReceiveCondition_observer:
+            self._onReceiveCondition_observer.append(func)
+
+    def removeOnReceiveCondition(self, func):
+        if func in self._onReceiveCondition_observer:
+            self._onReceiveCondition_observer.remove(func)
+
+    def addOnReceiveRealCondition(self, func):
+        if func not in self._onReceiveRealCondition_observer:
+            self._onReceiveRealCondition_observer.append(func)
+
+    def removeOnReceiveRealCondition(self, func):
+        if func in self._onReceiveRealCondition_observer:
+            self._onReceiveRealCondition_observer.remove(func)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import  *
+from PyQt5.QtGui import *
 from PyQt5 import uic
 from PyQt5.QAxContainer import *
+import datetime
 import kiwoomOS as KOS
 main_window = uic.loadUiType("main_window.ui")[0]
 
@@ -10,60 +12,168 @@ class MyWindow(QMainWindow, main_window):
         self.setupUi(self)
         self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
         self.kos = KOS.KiwoomOS(self.kiwoom)
+        self.kos.addOnLogin(self.kos_onLogin)
+        self.kos.addOnReceiveTr(self.kos_OnReceiveTr)
+        self.kos.addOnReceiveReal(self.kos_OnReceiveReal)
+        self.kos.addOnReceiveRealExt(self.kos_OnReceiveRealExt)
+        self.kos.addOnAcceptedOrder(self.kos_OnAcceptedOrder)
+        self.kos.addOnConcludedOrder(self.kos_OnConcludedOrder)
+        self.kos.addOnReceiveBalance(self.kos_OnReceiveBalance)
+        self.kos.addOnReceiveCondition(self.kos_OnReceiveCondition)
+        self.kos.addOnReceiveRealCondition(self.kos_OnReceiveRealCondition)
 
-        self.kiwoom.OnEventConnect.connect(self.api_OnEventConnect)  # 로그인 결과 Event
-        self.kiwoom.OnReceiveTrData.connect(self.api_OnReceiveTrData) # TR요청 수신결과 Event
-        self.kiwoom.OnReceiveRealData.connect(self.api_OnReceiveRealData) # 실시간 데이터 수신 Event
-        self.kiwoom.OnReceiveTrCondition.connect(self.api_OnReceiveTrCondition) # 조건검색 결과 Event
-        self.kiwoom.OnReceiveRealCondition.connect(self.api_OnReceiveRealCondition) #실시간 조건 수신 Event
-
+        #PyQt GUI Event
         self.loginButton.clicked.connect(self.login_button_clicked)
+        self.accountWindowButton.clicked.connect(self.account_window_button_clicked)
         self.requestTrButton.clicked.connect(self.request_tr_button_clicked)
+        self.continuousTrButton.clicked.connect(self.continuous_tr_button_clicked)
+        self.monitoringButton.clicked.connect(self.monitoring_button_clicked)
+
+        #TR예제 추가
+        self.trComboBox.addItem('종목기본정보요청_opt10001')
+        self.trComboBox.addItem('주식일봉차트조회요청_opt10081')
+
+        self.logModel = QStandardItemModel()
+        self.logListView.setModel(self.logModel)
+        self.logModel.appendRow(QStandardItem('로그창'))
 
     def login_button_clicked(self):
+        self.writeLog('로그인 버튼 클릭')
         self.kos.login()
 
+    def account_window_button_clicked(self):
+        self.writeLog('계좌비밀번호 버튼 클릭')
+        self.kos.showAccountWindow()
+
     def request_tr_button_clicked(self):
-        print('request_tr_button_clicked()')
-        self.kos.setInput('종목코드', '005930')
-        rqResult = self.kos.requestTr('종목기본정보', 'opt10001')
-        print('rqResult = ', rqResult)
+        self.writeLog('TR요청 버튼 클릭')
+        self.requestTR()
 
-    #로그인
-    def api_OnEventConnect(self, nErrCode):
-        print('nErrCode ', nErrCode)
-        if nErrCode == 0:
-            print('로그인 성공')
-            print(self.kos.getAccountList())
-            print(self.kos.getStockItemList())
-        else:
-            print('로그인 실패')
-    
-    #TR데이터 수신
-    def api_OnReceiveTrData(self, sScrNo, sRQName, sTrcode, sRecordName, sPrevNext, nDataLength, sErrorCode, sMessage, sSplmMsg):
-        print(sRQName)
-        cnt = self.kos.getTrCount(sTrcode)
-        print(cnt)
+    def continuous_tr_button_clicked(self):
+        self.writeLog('연속조회 버튼 클릭')
+        self.requestTR(2)
 
-        price = self.kos.getTrData(sTrcode, '현재가')
-        print('price :', price)
+    def requestTR(self, continuous=0):
+        rqName = self.trComboBox.currentText()
 
-    #실시간 데이터 수신
-    def api_OnReceiveRealData(self, sCode, sRealType, sRealData):
-        print(sCode, sRealType, sRealData)
+        if rqName == '종목기본정보요청_opt10001':
+            self.kos.setInput('종목코드', '005930')
+            rqResult = self.kos.requestTr('종목기본정보요청_opt10001', 'opt10001', continuous)
+            self.writeLog(rqName, '요청 시도 결과 =', rqResult)
 
-    #조건검색 결과 수신
-    def api_OnReceiveTrCondition(self, sScrNo, strCodeList, strConditionName, nIndex, nNext):
-        print(sScrNo, strCodeList, strConditionName, nIndex, nNext)
+        elif rqName == '주식일봉차트조회요청_opt10081':
+            self.kos.setInput('종목코드', '005930')
+            self.kos.setInput('기준일자', datetime.datetime.today().strftime('%Y%m%d'))
+            self.kos.setInput('수정주가구분', '1')
+            rqResult = self.kos.requestTr('주식일봉차트조회요청_opt10081', 'opt10081', continuous)
+            self.writeLog(rqName, '요청 시도 결과 =', rqResult)
 
-    #실시간 조건 편입/이탈 수신
-    def api_OnReceiveRealCondition(self, strCode, strType, strConditionName, strConditionIndex):
-        print(strCode, strType, strConditionName, strConditionIndex)
+    def monitoring_button_clicked(self):
+        self.writeLog('조건검색 버튼 클릭')
+        index = self.conditionComboBox.currentIndex()
+
+        if index > -1:
+            conditionIndex = self.conditionList[index]['조건식인덱스']
+            conditionName = self.conditionList[index]['조건식명']
+            self.kos.startConditionMonitoring(conditionName, conditionIndex)
+
+    #로그인 완료 Event
+    def kos_onLogin(self, stockItemList, conditionList):
+        self.writeLog('kos_onLogin()호출 - 로그인 이벤트')
+        self.stockItemList = stockItemList
+        self.conditionList = conditionList
+
+        self.writeLog('로그인 상태 :', self.kos.getLoginState())
+        self.writeLog('사용자 아이디 :', self.kos.getUserId())
+        self.writeLog('사용자 이름 :', self.kos.getUserName())
+        self.writeLog('서버구분 :', self.kos.getServerState())
+
+        for condition in conditionList:
+            self.conditionComboBox.addItem(condition['조건식명'])
+
+        for account in self.kos.getAccountList():
+            self.accountComboBox.addItem(account)
+
+    #TR수신 Event
+    def kos_OnReceiveTr(self, rqName, trCode, hasNext):
+        self.writeLog('kos_OnReceiveTr() 호출 - TR데이터 수신')
+        self.writeLog('rqName', rqName)
+        self.writeLog('trCode', trCode)
+        self.writeLog('hasNext', hasNext)
+
+        if rqName == '종목기본정보요청_opt10001':
+            itemCode = self.kos.getTrData(trCode, '종목코드')
+            itemName = self.kos.getTrData(trCode, '종목명')
+            price = self.kos.getTrData(trCode, '현재가')
+            changeRate = self.kos.getTrData(trCode, '등락율') #등락률이 맞으나 KOA Studio에 명시된 이름을 정확하게 입력해야 함
+            self.writeLog(itemCode, itemName, price, changeRate)
+            
+        elif rqName == '주식일봉차트조회요청_opt10081':
+            itemCount = self.kos.getTrCount(trCode)
+            itemCode = self.kos.getTrData(trCode, '종목코드')
+            self.writeLog('itemCount', itemCode)
+            self.writeLog(itemCode)
+            for i in range(itemCount):
+                date = self.kos.getTrData(trCode, '일자', i)
+                price = self.kos.getTrData(trCode, '현재가', i)
+                volume = self.kos.getTrData(trCode, '거래량', i)
+                checkPoint = self.kos.getTrData(trCode, '수정주가구분', i)
+                self.writeLog(date, price, volume, checkPoint)
+
+    #실시간 거래 데이터 수신
+    def kos_OnReceiveReal(self, itemCode, realData):
+        self.writeLog('kos_OnReceiveReal() 호출 - 실시간 데이터 수신')
+        self.writeLog(itemCode)
+        self.writeLog(realData)
+
+    #기타 실시가 데이터 수신
+    def kos_OnReceiveRealExt(self, itemCode, realType):
+        self.writeLog('kos_OnReceiveRealExt() 호출 - 기타 실시간 데이터 수신')
+        self.writeLog('realType', realType)
+        if realType == '주식당일거래원':
+            sellEx1 = self.kos.getRealData(itemCode, 141)
+            sellEx2 = self.kos.getRealData(itemCode, 142)
+            sellEx3 = self.kos.getRealData(itemCode, 143)
+            self.writeLog(sellEx1, sellEx2, sellEx3)
+
+    #주문 접수 Event
+    def kos_OnAcceptedOrder(self, receipt):
+        self.writeLog('kos_OnAcceptedOrder() 호출 - 주문접수 결과')
+        self.writeLog(receipt)
+
+    #주문 체결 Event
+    def kos_OnConcludedOrder(self, conclusion):
+        self.writeLog('kos_OnConcludedOrder() 호출 - 주문 체결 정보')
+        self.writeLog(conclusion)
+
+    #실시간 잔고 Event
+    def kos_OnReceiveBalance(self, balance):
+        self.writeLog('kos_kos_OnReceiveBalance() 호출 - 실시간 잔고 전달')
+        self.writeLog(balance)
+
+    #조건검색 종목 Event
+    def kos_OnReceiveCondition(self, condition, itemList):
+        self.writeLog('kos_OnReceiveCondition() 호출 - 조건검색 결과')
+        self.writeLog('condition', condition)
+        self.writeLog('itemList', itemList)
+
+    #실시간 조건검색 Event
+    def kos_OnReceiveRealCondition(self, condition, strCode, type):
+        self.writeLog('kos_OnReceiveRealCondition() 호출 - 실시간 조건 편입/이탈')
+        self.writeLog('condition', condition)
+        self.writeLog('type', type)
+        self.writeLog('strCode', strCode)
+
+    #LogListView에 로그 출력
+    def writeLog(self, *log):
+        logText = ''
+        for i in log:
+            logText += str(i) + ' '
+        self.logModel.appendRow(QStandardItem(logText))
 
 if __name__ == "__main__":
     print("프로그램 시작")
-
-    app = QApplication([]) #process
+    app = QApplication([])
     myWindow = MyWindow() #클래스를 이용해 객체를 만들고, 생성자(__init__())를 호출
     myWindow.show()
     app.exec_()
